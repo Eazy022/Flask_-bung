480b9cbd0bcca35cebc62abecd6a25b373830ad1
eazy022 <emanuel_friedrich@web.de>
2025-12-01 15:07:12 +0100

    workin logg in and out

--- website/views.py
+++ website/views.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request, render_template 
-from flask_login import current_user, login_required
+from flask_login import current_user, login_required, logout_user
 
 from . import db
 
@@ -9,12 +9,6 @@ views = Blueprint("views", __name__)
 @views.route("/", methods=["GET", "POST"])
 @login_required
 def home():
-    if request.method == "POST":
         return render_template("home.html", user=current_user)
-    else:
-        return render_template("login.html", user=current_user)
-
-@views.route("/logout")
-def Logout():
-        return  render_template("login.html", user=current_user)
+    
 
