0c57a2cc545f6a79888c5cc87edc6b3dc469bb7c
Eazy022 <emanuel_friedrich@web.de>
2025-12-02 10:10:02 +0100

    code fixes

--- website/auth.py
+++ website/auth.py
@@ -29,8 +29,9 @@ def login():
 
     return render_template ("login.html",user=current_user)
 
-@login_required
+
 @auth.route("/logout")
+@login_required
 def logout():  
      logout_user()
      flash("Logout successful.", category="success")
